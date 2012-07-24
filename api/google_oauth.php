cfc15eb31086d2448991c36b218f70df87571b31
Jim <jimdoescode@gmail.com>
2012-07-24 02:51:37 -0400

    Updated copyright year

--- api/google_oauth.php
+++ api/google_oauth.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (C) 2011 by Jim Saunders
+Copyright (C) 2012 by Jim Saunders
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- api/youtube.php
+++ api/youtube.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Copyright (C) 2011 by Jim Saunders
+Copyright (C) 2012 by Jim Saunders
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
